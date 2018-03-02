989b145ea85458a8fb83d3ba4cb2cb955f918d67
崇志 <chongzhi.ldq@taobao.com>
2018-03-02 13:37:16 +0800

    Fix: 重新mountView bugfix

--- hmr.js
+++ hmr.js
@@ -77,12 +77,16 @@ module.exports = (wsPort, cssSelectorPrefix, host) => {
                     }
                 }
 
-                currentVframes.forEach(function (vf) {
-                    // require 移除view模块缓存
-                    var path = seajs.resolve(vf.path);
+                // require 移除view模块缓存
+                pathObjs.depsPaths.forEach(function (_path) {
+                    var view = resolvePath2View(_path);
+                    var path = seajs.resolve(view);
                     delete seajs.cache[path];
                     delete seajs.data.fetchedList[path];
-                    // 重新加载view模块
+                })
+
+                // 重新加载view模块
+                currentVframes.forEach(function (vf) {
                     vf.mountView(vf.path, vf.viewInitParams)
                 })
             }
