7eecd01c02abc9a0ab6c96e71eae8a410049a57d
chongzhi.ldq <chongzhi.ldq@alibaba-inc.com>
2019-11-12 11:27:36 +0800

    Fix: magix-desiger独立出来，不再在hmr里注入

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const gulp = require('gulp')
 const path = require('path')
 const WebSocket = require('ws')
 const hmrjsfn = require('./hmr')
-const requestPromise = require('request-promise')
 
 module.exports = ({
     //热更新监听的文件
@@ -20,16 +19,13 @@ module.exports = ({
     ],
     //全局的样式，必须触发全页刷新
     scopedCss,
-    // cssSelectorPrefix,
     //可以自行指定注入到页面的hmr脚本
     hmrJs,
     rootAppName = 'app', //默认的项目app目录名
     //可以固定websocket的端口号，不自动生成
     wsPort,
-    closeDesiger,
     combineTool,
-    host = '127.0.0.1',
-    mdPort = 3007 //magix-desiger用的端口号，注入到页面上
+    host = '127.0.0.1'
 }, ws) => {
 
     if (wsPort) {
@@ -56,7 +52,8 @@ module.exports = ({
         })
 
         const watcher = gulp.watch(watchFiles)
-        watcher.on('change', (_filePath) => {
+
+        watcher.on('change', (_filePath, stats) => {
 
             let filePath = path.resolve(process.cwd(), _filePath)
             console.log('[HMR]', chalk.green('file changed'), chalk.cyan(filePath))
@@ -139,16 +136,6 @@ module.exports = ({
             throw new Error('路径：' + this.path + ' 对应的文件没有找到')
         }
 
-        //--closeDesiger 控制是否启用magix-desiger
-        let magixDesigerJs
-        if (!closeDesiger) {
-            //magix-desiger相关的js注入，保存在alp上面
-            magixDesigerJs = yield requestPromise({
-                url: 'https://mo.m.taobao.com/magix_desiger_page_version',
-                rejectUnauthorized: false
-            })
-        }
-
         //浏览器端的websocket代码
         host = host.replace(/^https?:\/\//, '')
         hmrJs = hmrJs || hmrjsfn(wsPort, host, rootAppName)
@@ -156,8 +143,6 @@ module.exports = ({
         //插入热更新所需要的js文件
         body = body.replace('</body>', `
             <script>${hmrJs}</script>
-            <!-- <magix-designer-port>${mdPort}</magix-designer-port> -->  
-            ${magixDesigerJs ? `<script src="${magixDesigerJs}"></script>` : ``}
             </body>
         `)
         this.body = body
